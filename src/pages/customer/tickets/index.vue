a1c090bc391c859d058e483ad33b2a5745973c2b
phyothiha <phyothiha96@gmail.com>
2024-06-01 20:44:09 +0630

    vue query: update query key and refetch

--- src/pages/customer/tickets/index.vue
+++ src/pages/customer/tickets/index.vue
@@ -18,8 +18,6 @@
           :items="serverItems"
           :items-length="totalItems"
           :loading="loading"
-          :search="search"
-          item-value="name"
           @update:options="loadItems"
         ></v-data-table-server>
       </template>
@@ -27,146 +25,72 @@
   </div>
 </template>
 
-<script>
-const desserts = [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    iron: "1",
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    iron: "0",
-  },
-  {
-    name: "KitKat",
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: "6",
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    iron: "7",
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    iron: "16",
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    iron: "1",
-  },
-  {
-    name: "Lollipop",
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    iron: "2",
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    iron: "8",
-  },
-  {
-    name: "Honeycomb",
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    iron: "45",
-  },
-  {
-    name: "Donut",
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    iron: "22",
-  },
-];
+<script setup>
+import { ref } from "vue";
+import { useQuery } from "@tanstack/vue-query";
+import axios from "axios";
 
-const FakeAPI = {
-  async fetch({ page, itemsPerPage, sortBy }) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const start = (page - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
-        const items = desserts.slice();
-
-        if (sortBy.length) {
-          const sortKey = sortBy[0].key;
-          const sortOrder = sortBy[0].order;
-          items.sort((a, b) => {
-            const aValue = a[sortKey];
-            const bValue = b[sortKey];
-            return sortOrder === "desc" ? bValue - aValue : aValue - bValue;
-          });
-        }
-
-        const paginated = items.slice(start, end);
-
-        resolve({ items: paginated, total: items.length });
-      }, 500);
-    });
+const itemsPerPage = ref(6);
+const headers = ref([
+  {
+    title: "No.",
+    align: "start",
+    sortable: false,
+    key: "id",
   },
-};
-
-export default {
-  data: () => ({
-    itemsPerPage: 5,
-    headers: [
-      {
-        title: "Dessert (100g serving)",
-        align: "start",
-        sortable: false,
-        key: "name",
-      },
-      { title: "Calories", key: "calories", align: "end" },
-      { title: "Fat (g)", key: "fat", align: "end" },
-      { title: "Carbs (g)", key: "carbs", align: "end" },
-      { title: "Protein (g)", key: "protein", align: "end" },
-      { title: "Iron (%)", key: "iron", align: "end" },
-    ],
-    search: "",
-    serverItems: [],
-    loading: true,
-    totalItems: 0,
-  }),
-  methods: {
-    loadItems({ page, itemsPerPage, sortBy }) {
-      this.loading = true;
-      FakeAPI.fetch({ page, itemsPerPage, sortBy }).then(({ items, total }) => {
-        this.serverItems = items;
-        this.totalItems = total;
-        this.loading = false;
-      });
-    },
+  {
+    title: "Email",
+    align: "start",
+    sortable: false,
+    key: "email",
   },
-};
+  {
+    title: "First Name",
+    align: "start",
+    sortable: false,
+    key: "first_name",
+  },
+  {
+    title: "Last Name",
+    align: "start",
+    sortable: false,
+    key: "last_name",
+  },
+]);
+const serverItems = ref([]);
+const totalItems = ref(0);
+const loading = ref(true);
+const currentPage = ref(1);
+
+async function fetchItems({ queryKey }) {
+  const [_key, { page }] = queryKey;
+
+  console.log(page);
+
+  try {
+    const { data } = await axios.get(
+      `https://reqres.in/api/users?page=${page}`
+    );
+
+    serverItems.value = data.data;
+    totalItems.value = data.total;
+
+    return Promise.resolve(data);
+  } catch (err) {
+    return Promise.reject(err);
+  } finally {
+    loading.value = false;
+  }
+}
+
+const { isPending, isFetching, isError, data, error, refetch } = useQuery({
+  queryKey: ["users", { page: currentPage.value }],
+  queryFn: fetchItems,
+});
+
+function loadItems({ page }) {
+  if (page > 1) {
+    currentPage.value = page;
+  }
+}
 </script>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -5,13 +5,11 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import pinia from '@/stores'
-import router from '@/router'
+import vuetify from "./vuetify";
+import pinia from "@/stores";
+import router from "@/router";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
-export function registerPlugins (app) {
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app) {
+  app.use(vuetify).use(router).use(pinia).use(VueQueryPlugin);
 }
